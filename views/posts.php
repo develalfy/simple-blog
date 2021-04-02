8603fd1c8b9a9e2bd9876304574308c541c48a3d
Ashraf Elalfy <ashraf.elalfy@areebgroup.com>
2021-04-02 18:00:10 +0200

    Add bootstrap

--- views/posts.php
+++ views/posts.php
@@ -1,23 +1,46 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Posts</title>
+    <meta charset="utf-8">
+    <title>Blog</title>
+    <!-- Bootstrap core CSS -->
+    <link href="../dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body>
-<?php foreach ($posts as $post): ?>
-    <div class="post">
-        <a href="/posts/<?= $this->e($post['id']) ?>">
-            <h1><?= $this->e($post['title']) ?></h1>
-        </a>
-        <p>
-            <?= $this->e($post['desc']) ?>
-        </p>
+
+<main role="main">
+
+    <section class="jumbotron text-center">
+        <div class="container">
+            <h1 class="jumbotron-heading">Blog</h1>
+            <p class="lead text-muted">Here we list just titles then you can view each post by clicking view button.</p>
+        </div>
+    </section>
+
+    <div class="album py-5">
+        <div class="container">
+
+            <div class="row">
+                <?php foreach ($posts as $post): ?>
+                    <div class="col-md-4">
+                        <div class="card mb-4 shadow-sm">
+                            <div class="card-body">
+                                <p class="card-text"><?= ucfirst($this->e($post['title'])) ?>.</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <a href="/posts/<?= $this->e($post['id']) ?>"
+                                           class="btn btn-block btn-outline-secondary">View</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
     </div>
-    <hr>
-<?php endforeach; ?>
+
+</main>
 </body>
 </html>
